5aa13cf85ed7e104d96f8cae9b91bd68e756c7fa
Daniel Briechle <danbriechle@hotmail.com>
2018-10-25 17:36:09 -0600

    Created merchant class with corresponding test all test passing

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,5 +1,5 @@
-require_relative '../lib/merchant'
 require_relative 'test_helper'
+require_relative '../lib/merchant'
 
 class MerchantTest < Minitest::Test
   def test_it_exists
@@ -9,27 +9,13 @@ class MerchantTest < Minitest::Test
   end
 
   def test_it_has_id
-    # skip
     m1 = Merchant.new({:id => 5, :name => "Turing School"})
-
     assert_equal 5, m1.id
   end
 
   def test_it_has_a_name
-    # skip
     m1 = Merchant.new({:id => 5, :name => "Turing School"})
     assert_equal "Turing School", m1.name
-  end
-
-  def test_collection_of_merchants
-    m1 = Merchant.new({:id => 5, :name => "Turing School"})
-
-    assert_equal m1.array_of_hashes, m1.collection_of_merchants
-    binding.pry
-  end
-
-  def test_merchants_collection_class_method
-
   end
 
 end
