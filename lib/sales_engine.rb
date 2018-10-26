9ccd5120fd2dfecb65a9358af636410079d3380e
Daniel Briechle <danbriechle@hotmail.com>
2018-10-26 14:11:01 -0600

    Created sales engine class and corresponding test, still a work in progress

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,22 @@
+require 'CSV'
+
+
 class SalesEngine
+  attr_reader :item_csv, :merchant_csv
+
+  def initialize(csv_files)
+    @csv_files =csv_files
+    # @item_csv = csv_files[:items]
+    # @merchant_csv = csv_files[:merchant]
+  
+  end
+
+
   def self.from_csv(csv_files)
+    @item_csv = csv_files[:items]
+    @merchant_csv = csv_files[:merchant]
+    ItemRepo.new(@item_csv)
+    MerchantRepo.new(@merchant_csv)
+    self.from_csv(@merchant_repo, @item_csv)
   end
 end
